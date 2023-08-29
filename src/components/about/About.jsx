7e922140e21b69462f2bd4843bc7b23aee8a1d5a
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-08-29 11:23:17 +0200

    updates

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import './about.css'
-import square from '../../assets/square.png'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {LuFolderCheck} from 'react-icons/lu'
+import React from "react";
+import "./about.css";
+import square from "../../assets/square.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { LuFolderCheck } from "react-icons/lu";
 
 const About = () => {
     return (
-        <section id='about'>
+        <section id="about">
             <h5>Get To Know</h5>
             <h2>About Me</h2>
 
@@ -20,35 +20,35 @@ const About = () => {
 
                 <div className="about__content">
                     <div className="about__cards">
-                        <article className='about__card'>
-                            <FaAward className='about__icon'/>
+                        <article className="about__card">
+                            <FaAward className="about__icon" />
                             <h5>Experience</h5>
                             <small>Solo projects</small>
                         </article>
 
-                        <article className='about__card'>
-                            <FiUsers className='about__icon'/>
-                            <h5>Clients</h5>
-                            <small>10+ Worldwide</small>
-                        </article>
-
-                        <article className='about__card'>
-                            <LuFolderCheck className='about__icon'/>
+                        <article className="about__card">
+                            <LuFolderCheck className="about__icon" />
                             <h5>Projects</h5>
-                            <small>Project for myself</small>
+                            <small>You can see my project on GitHub</small>
                         </article>
                     </div>
 
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quam doloremque 
-                        iste cupiditate rem voluptatem eos nobis placeat, repudiandae, error consequatur necessitatibus 
-                        iusto facilis inventore repellat, tenetur consequuntur eveniet labore!
+                    <p>
+                        Hi my name is Marian Remus Burlacu but u can just call
+                        me Remus. <br />
+                        I'm a front end developer base in Turin with a keen eye
+                        for designing good looking and user friendly web
+                        application. <br />I would like to expand my horizons by
+                        gaining work experience and living outside Italy
                     </p>
 
-                    <a href="#contacts" className="btn btn-primary">Let's Talk</a>
+                    <a href="#contacts" className="btn btn-primary">
+                        Let's Talk
+                    </a>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default About
+export default About;
